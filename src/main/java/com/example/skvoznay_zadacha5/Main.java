867e30138b0b2a4426c268608e8ee11fd157ea48
Dianaaaaaaaaaaaa <barinovadiana0@gmail.com>
2023-01-16 17:52:11 +0300

    commit

--- src/main/java/com/example/skvoznay_zadacha5/Main.java
+++ src/main/java/com/example/skvoznay_zadacha5/Main.java
@@ -1,23 +1,19 @@
 package com.example.skvoznay_zadacha5;
 
+import com.example.skvoznay_zadacha5.Work.WorkFile;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+public class Main  {
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) throws FileNotFoundException {
+        WorkFile workFile=new WorkFile();
+        workFile.Read("input.zip");
+        workFile.write("out.txt");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.skvoznay_zadacha5 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires javaluator;
+
+
     opens com.example.skvoznay_zadacha5 to javafx.fxml;
     exports com.example.skvoznay_zadacha5;
+    exports com.example.skvoznay_zadacha5.Work;
+    opens com.example.skvoznay_zadacha5.Work to javafx.fxml;
 }
